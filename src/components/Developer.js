fd6249530965f41b72b7cbdffc03e2b47eb19d17
alexissorianooo <alexis.soriano27@gmail.com>
2022-06-01 13:11:05 +0800

    developer notes edit

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Developer(){
     return(
-        <div className='relative bottom-0 top-9'>
+        <div className='relative bottom-0 top-9 flex flex-col justify-center items-center'>
             Developed by <span className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-purple-500'>Alexis Justin Soriano</span>
-            <p><a href="https://github.com/alexissorianooo/memegenerator" target="_blank" rel="noreferrer">View Code</a></p>
+            <p><a href="https://github.com/alexissorianooo/memegenerator" target="_blank" rel="noreferrer">Click me to view code</a></p>
         </div>
     )
 }
