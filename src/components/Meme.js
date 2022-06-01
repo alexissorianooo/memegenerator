7ca28311ca38e04b4723ba60319b73c67333e4a0
alexissorianooo <alexis.soriano27@gmail.com>
2022-06-01 13:04:16 +0800

    max witdh of text meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Meme(props){
     return(
         <div className='relative h-full lg:max-h-[800px]'>
-            <p className='memetext absolute top-4 left-1/2 -translate-x-1/2 md:text-5xl text-4xl md:w-[500px]'>
+            <p className='memetext absolute top-4 left-1/2 -translate-x-1/2 md:text-5xl text-4xl md:max-w-[500px]'>
                 {props.memetop}
             </p>
-            <p className='memetext absolute bottom-4 left-1/2 -translate-x-1/2 md:text-5xl text-4xl md:w-[500px]'>
+            <p className='memetext absolute bottom-4 left-1/2 -translate-x-1/2 md:text-5xl text-4xl md:max-w-[500px]'>
                 {props.memebottom}
             </p>
             <img src={props.memeSource} alt={props.memeName} className='object-scale-down m-auto max-h-[800px]'/>
